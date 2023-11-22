af042141d2061dfa1fc90b08f69de679cc889ed1
pcih <pcih1984@gmail.com>
2023-11-22 22:00:19 +0300

    lesson_3 Template's context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.views.generic import TemplateView
 
 
@@ -7,6 +8,16 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        return context
 
 
 class CoursesPageView(TemplateView):
